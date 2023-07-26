33a59fc0f67a0e6c0f998d391e34e9169991b20a
miguel.martin <miguel.martin@agetic.gob.bo>
2023-07-26 14:02:48 -0400

    chore: move todo comments opt

--- lua/custom/configs/overrides.lua
+++ lua/custom/configs/overrides.lua
@@ -80,4 +80,47 @@ M.nvimtree = {
   },
 }
 
+M.todo_comments = {
+  signs = true,
+  sign_priority = 8,
+  keywords = {
+    FIX = { icon = " ", color = "#C34043", alt = { "FIXME", "BUG", "FIXIT", "ISSUE" } },
+    TODO = { icon = " ", color = "info" },
+    DONE = { icon = " ", color = "done", alt = { "COMPLETE" } },
+    HACK = { icon = " ", color = "warning" },
+    WARN = { icon = " ", color = "error", alt = { "WARNING", "XXX" } },
+    PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
+    NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
+  },
+  merge_keywords = true, -- when true, custom keywords will be merged with the defaults
+  highlight = {
+    before = "", -- "fg" or "bg" or empty
+    keyword = "wide", -- "fg", "bg", "wide" or empty.
+    after = "fg", -- "fg" or "bg" or empty
+    pattern = [[.*<(KEYWORDS)\s*:]], -- pattern or table of patterns, used for highlightng (vim regex)
+    comments_only = true, -- uses treesitter to match keywords in comments only
+    max_line_len = 400, -- ignore lines longer than this
+    exclude = {}, -- list of file types to exclude highlighting
+  },
+  colors = {
+    error = { "DiagnosticError", "ErrorMsg", "#DC2626" },
+    warning = { "DiagnosticWarning", "WarningMsg", "#FBBF24" },
+    info = { "DiagnosticInfo", "#7FB4CA" },
+    done = { "DiagnosticDone", "#00A600" },
+    hint = { "DiagnosticHint", "#10B981" },
+    default = { "Identifier", "#C34043" },
+  },
+  search = {
+    command = "rg",
+    args = {
+      "--color=never",
+      "--no-heading",
+      "--with-filename",
+      "--line-number",
+      "--column",
+    },
+    pattern = [[\b(KEYWORDS):]], -- ripgrep regex
+  },
+}
+
 return M
--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -124,49 +124,8 @@ local plugins = {
     "folke/todo-comments.nvim",
     cmd = { "TodoTrouble", "TodoTelescope" },
     event = "BufReadPost",
-    opts = {
-      signs = true,
-      sign_priority = 8,
-      keywords = {
-        FIX = { icon = " ", color = "#C34043", alt = { "FIXME", "BUG", "FIXIT", "ISSUE" } },
-        TODO = { icon = " ", color = "info" },
-        DONE = { icon = " ", color = "done", alt = { "COMPLETE" } },
-        HACK = { icon = " ", color = "warning" },
-        WARN = { icon = " ", color = "error", alt = { "WARNING", "XXX" } },
-        PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
-        NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
-      },
-      merge_keywords = true, -- when true, custom keywords will be merged with the defaults
-      highlight = {
-        before = "", -- "fg" or "bg" or empty
-        keyword = "wide", -- "fg", "bg", "wide" or empty.
-        after = "fg", -- "fg" or "bg" or empty
-        pattern = [[.*<(KEYWORDS)\s*:]], -- pattern or table of patterns, used for highlightng (vim regex)
-        comments_only = true, -- uses treesitter to match keywords in comments only
-        max_line_len = 400, -- ignore lines longer than this
-        exclude = {}, -- list of file types to exclude highlighting
-      },
-      colors = {
-        error = { "DiagnosticError", "ErrorMsg", "#DC2626" },
-        warning = { "DiagnosticWarning", "WarningMsg", "#FBBF24" },
-        info = { "DiagnosticInfo", "#7FB4CA" },
-        done = { "DiagnosticDone", "#00A600" },
-        hint = { "DiagnosticHint", "#10B981" },
-        default = { "Identifier", "#C34043" },
-      },
-      search = {
-        command = "rg",
-        args = {
-          "--color=never",
-          "--no-heading",
-          "--with-filename",
-          "--line-number",
-          "--column",
-        },
-        pattern = [[\b(KEYWORDS):]], -- ripgrep regex
-      },
-    },
-      config = function(_, opts)
+    opts = overrides.todo_comments,
+    config = function(_, opts)
       require("todo-comments").setup(opts)
     end,
   },
@@ -184,6 +143,7 @@ local plugins = {
       }
     end,
   },
+
   {
     "kevinhwang91/nvim-bqf",
     lazy = false,
